5ac571c963c59cadbcb9e725e30b9ad38606cd6c
devnydv <bittumailb2@gmail.com>
2025-07-21 00:30:16 +0530

    Add .gitignore to exclude Python-generated files and virtual environments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from urllib import response
+
 from flask import Flask, request, jsonify, render_template
 from supabase import create_client, Client
 app = Flask(__name__)
@@ -8,8 +8,42 @@ key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI
 supabase: Client = create_client(url, key)
 @app.route('/', methods=['POST',"GET"])
 def process_data():
-    response = supabase.table('data').select("*").limit(10).execute()
-    
-    return render_template('home.html', data=response.data)
+    count_response = supabase.table("data").select("*", count="exact").execute()
+    # response = supabase.table('data').select("*").limit(10).execute()
+    start = count_response.count
+    last_seen_id = 20
+    response = (
+    supabase
+    .table("data")
+    .select("*")
+    .lt("id", last_seen_id)
+    .order("id", desc=True)
+    .limit(10)
+    .execute()
+)
+    # response = (
+    # supabase
+    # .table("data")
+    # .select("*")
+    # .order("id", desc=True)
+  #   .range(start -9, start)
+  #   .execute()
+  # )
+
+    return render_template('home.html', data=response.data, total_count=count_response.count)
+
+@app.route('/data', methods=['POST',"GET"])
+def data():
+    last_seen_id = 20
+    response = (
+    supabase
+    .table("data")
+    .select("*")
+    .lt("id", 30)
+    .order("id", desc=True)
+    .limit(10)
+    .execute()
+)
+    return jsonify(response.data)
 if __name__ == '__main__':
     app.run(debug=True)
